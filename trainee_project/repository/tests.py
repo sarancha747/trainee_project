d98d2072d53dce291f42c8a1030441b4707be146
sarancha747 <sarancha747@gmail.com>
2021-05-27 10:58:47 +0300

    Test file refactoring

--- trainee_project/repository/tests.py
+++ trainee_project/repository/tests.py
@@ -20,46 +20,49 @@ class FileSaveTestCase(TestCase):
         self.file_dir = os.path.join(os.path.dirname(settings.PROJECT_DIR), settings.FILE_DIR)
         self.test_file_name_1 = 'equal_file_1.txt'
         self.test_file_name_2 = 'equal_file_2.txt'
+        self.first_file_and_user = {
+            'username': 'testuser1',
+            'password': 'password',
+            'title': '1_test_title',
+            'description': '1_test_description',
+            'test_file_name': 'equal_file_1.txt'
+        }
+        self.second_file_and_user = {
+            'username': 'testuser2',
+            'password': 'password',
+            'title': '2_test_title',
+            'description': '2_test_description',
+            'test_file_name': 'equal_file_2.txt'
+        }
 
     def test_1_one_file_save(self):
-        def add_first_file():
-            self.client.login(username='testuser1', password="password")
+        def add_file_for_user(file_and_user):
+            self.client.login(username=file_and_user['username'], password=file_and_user['password'])
             get_response = self.client.get('')
             self.assertEqual(200, get_response.status_code)
-            with open(os.path.join(self.test_file_dir, self.test_file_name_1), 'rb') as test_file_1:
-                data = {'title': '1_test_title', 'description': '1_test_description', 'upload': test_file_1}
+            with open(os.path.join(self.test_file_dir, file_and_user['test_file_name']), 'rb') as test_file_1:
+                data = {'title': file_and_user['title'], 'description': file_and_user['description'],
+                        'upload': test_file_1}
                 post_response = self.client.post('', data)
                 self.assertEqual(200, post_response.status_code)
-            file = File.objects.get(title='1_test_title')
-            self.assertEqual('1_test_title', file.title)
-            self.assertEqual('1_test_description', file.description)
-            self.assertEqual('equal_file_1.txt', file.user_file_title)
+            file = File.objects.get(title=file_and_user['title'])
+            self.assertEqual(file_and_user['title'], file.title)
+            self.assertEqual(file_and_user['description'], file.description)
+            self.assertEqual(file_and_user['test_file_name'], file.user_file_title)
 
-        def add_second_file():
-            self.client.login(username='testuser2', password="password")
-            get_response = self.client.get('')
-            self.assertEqual(200, get_response.status_code)
-            with open(os.path.join(self.test_file_dir, self.test_file_name_2), 'rb') as test_file_2:
-                data = {'title': '2_test_title', 'description': '2_test_description', 'upload': test_file_2}
-                post_response = self.client.post('', data)
-                self.assertEqual(200, post_response.status_code)
-            file = File.objects.get(title='2_test_title')
-            self.assertEqual('2_test_title', file.title)
-            self.assertEqual('2_test_description', file.description)
-            self.assertEqual('equal_file_2.txt', file.user_file_title)
+        add_file_for_user(self.first_file_and_user)
+        add_file_for_user(self.second_file_and_user)
 
-        add_first_file()
-        add_second_file()
         # Проверяем что хеши у двух юзеров одинаковые
-        first_hash = File.objects.get(title='1_test_title')
-        second_hash = File.objects.get(title='2_test_title')
+        first_hash = File.objects.get(title=self.first_file_and_user['title'])
+        second_hash = File.objects.get(title=self.second_file_and_user['title'])
         self.assertEqual(first_hash.upload.file_hash, second_hash.upload.file_hash)
 
         # Проверяем что один файл в папке
-        self.assertIn(self.test_file_name_1, os.listdir(self.file_dir))
+        self.assertIn(self.first_file_and_user['test_file_name'], os.listdir(self.file_dir))
         self.assertEqual(len(os.listdir(self.file_dir)), 1)
 
-    def test_2_file_folder_clean(self):
+    def tearDown(self):
         for filename in os.listdir(self.file_dir):
             file_path = os.path.join(self.file_dir, filename)
             try:
